f160a0396dbbf2d5f917d240d84161f72b3af5c6
jeremie rousseau <jere.rousseau@gmail.com>
2019-11-20 17:12:38 +0100

    exercice finished. added a navbar with bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Ramen-Ya',
+    address:      'Place Commerces, Nantes',
+    category:     'japanese'
+  },
+  {
+    name:         'Le Petit François',
+    address:      'Ile de Nantes, Nantes',
+    category:     'french'
+  },
+  {
+    name:         'Yum-Yum',
+    address:      'Centre, Nantes',
+    category:     'chinese'
+  },
+  {
+    name:         'Gargamelle',
+    address:      'Place de la gare, Clisson',
+    category:     'italian'
+  },
+  {
+    name:         "L'baraque à frites",
+    address:      'Grand Place, Lille',
+    category:     'french'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
